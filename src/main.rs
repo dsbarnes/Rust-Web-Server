a88ff432e806d1978d6c8b9d79b4b4a941b858f4
Derek Barnes <dsbarnes@Dereks-iMac.attlocal.net>
2020-08-26 17:48:34 -0400

    added /sleep route

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 
 fn main() {
@@ -18,22 +20,20 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep){
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        let status = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let response = format!("{}{}", status, contents);
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status, contents);
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
-
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
